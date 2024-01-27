37d2aaf282a82d523e64d4790208f39b04b38626
dipeit@gmail.com <dipeit@gmail.com>
2024-01-27 03:48:35 +0000

    Created ServerModule1; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,10 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
 
 class Form1(Form1Template):
   def __init__(self, **properties):
